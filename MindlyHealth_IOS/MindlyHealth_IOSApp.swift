7ca090abe759a49d2eea1673953d6647091da711
michellempi <mmercyphe@student.ciputra.ac.id>
2025-05-21 08:09:07 +0700

    Initial Commit

---
+++ MindlyHealth_IOS/MindlyHealth_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindlyHealth_IOSApp.swift
+//  MindlyHealth_IOS
+//
+//  Created by Michelle Wijaya on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MindlyHealth_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindlyHealth_IOSTests/MindlyHealth_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindlyHealth_IOSTests.swift
+//  MindlyHealth_IOSTests
+//
+//  Created by Michelle Wijaya on 21/05/25.
+//
+
+import Testing
+@testable import MindlyHealth_IOS
+
+struct MindlyHealth_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
